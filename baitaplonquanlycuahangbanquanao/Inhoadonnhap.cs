2f3935b991c6c4f8daf66f70d77eba14d27db551
Admin <Admin@DESKTOP-N0GLJ1N>
2025-03-19 16:10:47 +0700

    Add project files.

---
+++ baitaplonquanlycuahangbanquanao/Inhoadonnhap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baitaplonquanlycuahangbanquanao
+{
+    public partial class Inhoadonnhap : Form
+    {
+        public Inhoadonnhap()
+        {
+            InitializeComponent();
+        }
+
+        private void Inhoadonnhap_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
